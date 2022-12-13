b2e9bb47485e9b12035f7c0108472d96376b64ae
khidirbekov <tacata97@live.com>
2022-12-13 16:12:07 +0300

    feat(App): add date in p

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className='App-logo' alt='logo' />
         <p>
           Hello this is ci/cd app test
+          <p>13.12.2022</p>
         </p>
         <h1>: )</h1>
       </header>
